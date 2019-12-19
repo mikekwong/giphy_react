fadfa39a18794a3f7760ac29bd00ca3c7cbe64e6
Mike Wong <wongmdesign@gmail.com>
2019-12-18 22:35:06 -0500

    individual gif endpoint working

--- src/components/Results/ResultDetail.js
+++ src/components/Results/ResultDetail.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import giphy, { API_KEY } from "../../api/giphy";
 
-export default () => {
-  return (
-    <div>
-      <p>Hello</p>
-    </div>
-  );
-};
+export default class ResultDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      metaData: [],
+      isLoading: true,
+      error: null
+    };
+  }
+
+  async componentDidMount() {
+    const { id } = this.props.match.params;
+
+    try {
+      const { data } = await giphy.get(`/gifs/${id}?api_key=${API_KEY}`);
+      this.setState({
+        metaData: data.data,
+        isLoading: false
+      });
+    } catch (error) {
+      this.setState({
+        error,
+        isLoading: false
+      });
+    }
+  }
+
+  render() {
+    const {
+      metaData: { source, title }
+    } = this.state;
+    console.log(this.state.metaData);
+    return (
+      <div>
+        <p>{title}</p>
+        <img src={source} />
+      </div>
+    );
+  }
+}
